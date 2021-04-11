aca6eea64f01f77052752216fdaac4cde84f3267
subhankar <sub.nitt@yahoo.com>
2021-04-11 21:06:13 +0530

    first commit

---
+++ src/main/java/com/example/bank/demo/Bank.java
@@ -0,0 +1,44 @@
+package com.example.bank.demo;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@javax.persistence.Entity
+@Table(name = "bank")
+public class Bank{
+
+    @Id
+    private String accountNumber;
+    private String holderName;
+    
+
+
+    /**
+     * @return String return the accountNumber
+     */
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * @param accountNumber the accountNumber to set
+     */
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     * @return String return the holderName
+     */
+    public String getHolderName() {
+        return holderName;
+    }
+
+    /**
+     * @param holderName the holderName to set
+     */
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+}
